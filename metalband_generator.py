9439301f346238d8c214a65c62b687c5dccae6a0
Natasha Umer <natasha.umer2@gmail.com>
2016-08-13 11:54:21 -0400

    random metal band name generator

--- metalband_generator.py
+++ metalband_generator.py
@@ -1,41 +1,39 @@
-
-# A program that generates random metal band names
-
-""" Earlier today, in the Recurse Center Slack Room, one of my mentors jokingly suggested
+"""
+Earlier today, in the Recurse Center Slack Room, one of my mentors jokingly suggested
 a project idea for a "cool metal band generator." I thought that was a fun idea,
-so I made one. Whether the names generated are cool or not is up for debate. Some examples:
-asses vile
-enfeeble revocation
-dominie dying fetus
-overexercise nile
-brain drill meathook
+so I made one. Whether the names generated are cool or not is up for debate.
+
+Some examples:
+* asses vile
+* enfeeble revocation
+* dominie dying fetus
+* overexercise nile
+* brain drill meathook
+
 """
 
 import random
 
-def make_list(stringHere):
-    fin = open(stringHere)
-    t = []
-    for line in fin:
-        words = line.strip()
-        t+=[words]
-    fin.close()
-    return t
+def make_list(filehere):
+    with open(filehere) as infile:
+        lines = infile.readlines()
+
+        return [l.strip() for l in lines]
 
 
 
+def metal_bands(names_generated=1):
+    for _ in range(names_generated):
+        metal_word = random.choice(make_list("metalbands.txt")).lower()
 
-def metal_bands(words_generated=1):
-    for phrases in range(words_generated):
-        metal_word = random.choice(make_list("metalbands.txt"))
-        question = input("Do you want to choose a word? Type YES or NO: ")
+        question = input("Do you want to choose a word? Type YES/NO: ").upper()
         if question.upper() == "YES":
             user_word = input("Type a random word: ")
-            print(user_word + " " + metal_word.lower())
+            print(user_word + " " + metal_word)
         else:
             random_word = random.choice(make_list("words.txt"))
             print(random_word + " " + metal_word.lower())
 
-metal_bands()
 
-# fin.close()
+if __name__ == '__main__':
+    metal_bands()
